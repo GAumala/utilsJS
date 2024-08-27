4a41049242b5901d18d8eb5bb3241bd642e149c5
Gabriel Aumala <gaumala@protonmail.com>
2024-08-27 14:17:09 -0500

    Add areObjectsEqual()

--- src/object.js
+++ src/object.js
@@ -67,4 +67,65 @@ const parseIntKeys = (obj, ...intKeys) => {
  */
 const isValidObject = (obj) => typeof obj === "object" && obj !== null;
 
-module.exports = { isValidObject, parseIntKeys, removeKeys, renameKeys };
+/**
+ * Checks deep equality between two objects comparing every key that has
+ * primitive values or objects. Functions are ignored, but if a key in the
+ * first object does have a function value, then the second object must
+ * also have a function value in that key for them to be equal.
+ * @param {object} obj1 - An object
+ * @param {object} obj2 - An object
+ * @returns {boolean} true if both objects have the same contents.
+ */
+const areObjectsEqual = (obj1, obj2) => {
+  if (typeof obj1 !== "object" || typeof obj2 !== "object") {
+    return false;
+  }
+
+  if (obj1 === null) {
+    return obj2 === null;
+  }
+
+  if (obj2 === null) {
+    return obj1 === null;
+  }
+
+  const keys = Object.keys(obj1);
+  if (keys.length !== Object.keys(obj2).length) {
+    return false;
+  }
+
+  for (let i = 0; i < keys.length; i++) {
+    const k = keys[i];
+    const attr1 = obj1[k];
+    const attr2 = obj2[k];
+
+    const type1 = typeof attr1;
+    const type2 = typeof attr2;
+
+    if (type1 !== type2) {
+      return false;
+    }
+
+    if (type1 === "number" || type1 === "boolean" || type1 === "string") {
+      if (attr1 !== attr2) {
+        return false;
+      }
+    }
+
+    if (type1 === "object") {
+      if (!areObjectsEqual(attr1, attr2)) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+};
+
+module.exports = {
+  areObjectsEqual,
+  isValidObject,
+  parseIntKeys,
+  removeKeys,
+  renameKeys,
+};
--- src/object.test.js
+++ src/object.test.js
@@ -1,5 +1,10 @@
 /* eslint-env jest */
-const { parseIntKeys, removeKeys, renameKeys } = require("./object.js");
+const {
+  areObjectsEqual,
+  parseIntKeys,
+  removeKeys,
+  renameKeys,
+} = require("./object.js");
 
 describe("parseIntKeys", () => {
   it("should parse int keys", () => {
@@ -20,5 +25,45 @@ describe("renameKeys", () => {
       e: 2,
       f: 3,
     });
+  });
+});
+
+describe("areObjectsEqual", () => {
+  it("should return true for two empty objects", () => {
+    expect(areObjectsEqual({}, {})).toBe(true);
+  });
+  it("should return true for two null objects", () => {
+    expect(areObjectsEqual(null, null)).toBe(true);
+  });
+  it("should return false if only one object is null", () => {
+    expect(areObjectsEqual(null, {})).toBe(false);
+    expect(areObjectsEqual({}, null)).toBe(false);
+  });
+  it("should return true if all primitive keys are equal", () => {
+    expect(
+      areObjectsEqual({ a: true, b: "two", c: 3 }, { c: 3, b: "two", a: true }),
+    ).toBe(true);
+  });
+  it("should ignore functions and return true if all primitive keys are equal", () => {
+    expect(
+      areObjectsEqual(
+        { a: true, b: "two", c: () => console.log("I like a") },
+        { c: () => console.log("i like b"), b: "two", a: true },
+      ),
+    ).toBe(true);
+  });
+  it("should check objects recursively", () => {
+    expect(
+      areObjectsEqual(
+        { a: true, b: "two", c: { d: 4 } },
+        { a: true, b: "two", c: { d: 4 } },
+      ),
+    ).toBe(true);
+    expect(
+      areObjectsEqual(
+        { a: true, b: "two", c: { d: 4 } },
+        { a: true, b: "two", c: { d: 3 } },
+      ),
+    ).toBe(false);
   });
 });
